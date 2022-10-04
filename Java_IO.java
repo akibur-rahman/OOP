cebe8fe7cbb5880587fbc4c2e2d37e1d4f477b71
Akibur Rahman <me.akiburrahman@gmail.com>
2022-10-04 11:23:03 +0600

    java input and output

---
+++ Java_IO.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Java_IO {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // String str = scanner.next();
+        // int number = scanner.nextInt();
+        // System.out.println("myString is: " + str);
+        // System.out.println("myInt is: " + number);
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        int num3 = scanner.nextInt();
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+    }
+}
