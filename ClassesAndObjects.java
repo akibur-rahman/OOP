32b7be5e5523e13fb72711b1aea48e654f13c329
Akibur Rahman <me.akiburrahman@gmail.com>
2022-10-20 18:22:57 +0600

    Classes and objects

---
+++ ClassesAndObjects.java
@@ -0,0 +1,23 @@
+
+class Student
+{
+    String name;
+    int id;
+    float cgpa;
+
+    public static void shout()
+    {
+        System.out.println("I can do anything.");
+    }
+}
+
+public class ClassesAndObjects {
+
+    public static void main(String[] args) {
+        Student akib = new Student();
+        akib.name = "Akibur Rahman";
+        akib.id = 213002094;
+        akib.cgpa = 3.08f;
+        System.out.println("Akib's Details are:\n" + akib.name +"\n"+ akib.id+"\n" + akib.cgpa+ "\n");
+    }
+}
--- VariableArguments.java
+++ VariableArguments.java
@@ -1,5 +1,11 @@
 public class VariableArguments {
 
+    /*
+     * Variable Arguments helps us to work with variable size of arguments in methods
+     * It takes all the arguments and put it inside a array pass it
+     * we write it as data_type...variable
+     */
+
     static int sum(int... arr) {
         int result = 0;
         for (int a : arr) {
