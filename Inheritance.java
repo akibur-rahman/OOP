2afab4e9e5d970bbe269ea3c7dbcac6f4cba652d
Akibur Rahman <me.akiburrahman@gmail.com>
2022-12-17 05:57:40 +0600

    inheritance (basic)

--- Inheritance.java
+++ Inheritance.java
@@ -1,37 +1,30 @@
 
-class Employee {
-    private String name;
-    private String id;
-    
-    public void setName(String name)
-    {
-        this.name = name;
+class Phone {
+    void call() {
+        System.out.println("Calling!");
     }
 
-    public String getName()
-    {
-        return name;
+    void textMessage() {
+        System.out.println("Texting!");
+    }
+}
+
+class SmartPhone extends Phone {
+    void runApps() {
+        System.out.println("Running YouTube!");
     }
 
-    public void setID(String id)
-    {
-        this.id = id;
-    }
-    
-    public String getID()
-    {
-        return id;
+    void videChat() {
+        System.out.println("Video Calling!");
     }
 }
 
 public class Inheritance {
     public static void main(String[] args) {
-        Employee akib = new Employee();
-        // akib.name = "Akib";
-        // akib.id = "213002094";
-        // System.out.println(akib.name +" "+ akib.id);
-        akib.setID("213002094");
-        akib.setName("Akibur Rahman");
-        System.out.println(akib.getID()+" "+akib.getName());
+        SmartPhone iPhone = new SmartPhone();
+        iPhone.call();
+        iPhone.textMessage();
+        iPhone.runApps();
+        iPhone.videChat();
     }
 }
