d3f62ecf640cc52326231756bbd256b386156e0b
Akibur Rahman <me.akiburrahman@gmail.com>
2022-12-23 23:04:41 +0600

    created java project about driving license test

---
+++ Project/Driving_License_Test/src/main/java/com/driving/driving_license_test/Driving_License_Test.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.driving.driving_license_test;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Driving_License_Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
