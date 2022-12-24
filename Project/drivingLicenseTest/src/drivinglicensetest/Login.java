0793c2af5b2f50f2046d775c32d2df1887b30033
Akibur Rahman <me.akiburrahman@gmail.com>
2022-12-24 10:18:25 +0600

    updated project

---
+++ Project/drivingLicenseTest/src/drivinglicensetest/Login.java
@@ -0,0 +1,19 @@
+package drivinglicensetest;
+
+import javax.swing.*;
+
+
+
+public class Login extends JFrame{
+    
+    Login()
+    {
+        setVisible(true);
+        setSize(600, 450);
+        setLocation(700, 300);
+    }
+    
+    public static void main(String[] args) {
+        new Login();
+    }
+}
