b4059d56374162a3656cf21a21924c81a4d96bb8
Akibur Rahman <me.akiburrahman@gmail.com>
2023-01-08 23:06:16 +0600

    score update

--- Project/drivingLicenseTest/src/drivinglicensetest/Score.java
+++ Project/drivingLicenseTest/src/drivinglicensetest/Score.java
@@ -8,11 +8,10 @@ public class Score extends JFrame implements ActionListener {
 
     Score(String name, int score) {
         setBounds(400, 150, 750, 550);
-        getContentPane().setBackground(Color.WHITE);
         setLayout(null);
         
         
-        JLabel heading = new JLabel("Thankyou " + name + " for playing Simple Minds");
+        JLabel heading = new JLabel("Thank You");
         heading.setBounds(45, 30, 700, 30);
         heading.setFont(new Font("Tahoma", Font.PLAIN, 26));
         add(heading);
